8466b5dbcc1495c62e41caffed3e924e3738e856
dzh0n <plotnikov.evgeniy@gmail.com>
2019-03-17 17:46:36 +0500

    work

--- platforms/browser/www/assets/js/view.js
+++ platforms/browser/www/assets/js/view.js
@@ -66,6 +66,12 @@ document.addEventListener("deviceready", function(){
 
     loadOrder();
 
+    window.plugins.OneSignal
+        .startInit("545e0ae9-f360-47cb-be70-8c8bab0ac2cc")
+        .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+        .handleNotificationOpened(notificationOpenedCallback)
+        .endInit();
+
 });
 
 function back() {
